278016becbef08f1a5c40105a12c2e86d102c8af
zhangzhe <zz950702@163.com>
2019-11-07 00:35:17 +0800

    consul 对接

--- demo-server/src/main/java/com/zhe/study/demo/server/DemoApplication.java
+++ demo-server/src/main/java/com/zhe/study/demo/server/DemoApplication.java
@@ -2,14 +2,15 @@ package com.zhe.study.demo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author zhangzhe
  */
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
 
