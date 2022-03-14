964bddc4c68b59444277851f2a8d62e61228b5bc
zhangzhe <zz950702@163.com>
2022-03-14 19:34:53 +0800

    i18n

---
+++ i18n-demo/src/main/java/com/zhe/i18n/server/controller/HelloController.java
@@ -0,0 +1,35 @@
+package com.zhe.i18n.server.controller;
+
+import com.zhe.i18n.common.HelloUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private HelloUtil helloUtil;
+
+    @GetMapping("/hello")
+    public String hello() {
+        Locale locale = LocaleContextHolder.getLocale();
+
+        String msg = messageSource.getMessage("current.local", null, locale);
+
+        return msg;
+    }
+
+    @GetMapping("/hello1")
+    public String hello1() {
+        return helloUtil.get();
+    }
+
+}
