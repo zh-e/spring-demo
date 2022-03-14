910fd70241e183e480c2a25bea4aff40d2b6d969
zhangzhe <zz950702@163.com>
2022-03-14 16:01:29 +0800

    日志组件

---
+++ demo-log/src/main/java/com/zhe/study/demo/log/Application.java
@@ -0,0 +1,13 @@
+package com.zhe.study.demo.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
