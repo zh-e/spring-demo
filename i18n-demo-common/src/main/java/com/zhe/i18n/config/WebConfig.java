4581067426877f19bb0b7160ca3d7bfea2ec1ee1
zh-e <zz950702@163.com>
2022-10-15 17:54:35 +0800

    i18n

--- i18n-demo-common/src/main/java/com/zhe/i18n/config/WebConfig.java
+++ i18n-demo-common/src/main/java/com/zhe/i18n/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.zhe.i18n.server.config;
+package com.zhe.i18n.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
