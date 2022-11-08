e37adb167fb62f350a6eeca20b8bf880f88ec929
zhangzhe <zz950702@163.com>
2022-11-08 19:10:35 +0800

    日志文件设置

--- api-base/src/main/java/com/zhe/api/base/controller/DemoController.java
+++ api-base/src/main/java/com/zhe/api/base/controller/DemoController.java
@@ -1,10 +1,9 @@
 package com.zhe.api.base.controller;
 
 import com.zhe.api.base.bean.BaseResponse;
-import com.zhe.api.base.bean.ValueDemo;
 import com.zhe.api.base.constant.RespCodeEnum;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/demo")
 public class DemoController {
 
-   @Autowired
-   private ValueDemo value;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
 
     @GetMapping("/get")
     public BaseResponse<String> get() {
-        return new BaseResponse<>(RespCodeEnum.SUCCESS, value.toString());
+        LOGGER.debug("debug log");
+        LOGGER.info("info log");
+        LOGGER.error("error log");
+        return new BaseResponse<>(RespCodeEnum.SUCCESS, "success");
     }
 
 
