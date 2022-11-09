caf9c08af79fbc5a0189b6e0b19082dceae05fd8
zh-e <zz950702@163.com>
2022-11-09 22:19:57 +0800

    api 请求demo示例

--- api-base/src/main/java/com/zhe/api/base/controller/DemoController.java
+++ api-base/src/main/java/com/zhe/api/base/controller/DemoController.java
@@ -1,12 +1,11 @@
 package com.zhe.api.base.controller;
 
 import com.zhe.api.base.bean.BaseResponse;
+import com.zhe.api.base.bean.DemoReq;
 import com.zhe.api.base.constant.RespCodeEnum;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/demo")
@@ -14,13 +13,29 @@ public class DemoController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DemoController.class);
 
-    @GetMapping("/get")
-    public BaseResponse<String> get() {
-        LOGGER.debug("debug log");
-        LOGGER.info("info log");
-        LOGGER.error("error log");
-        return new BaseResponse<>(RespCodeEnum.SUCCESS, "success");
+    @GetMapping("/get1")
+    public BaseResponse<String> get1() {
+        return new BaseResponse<>(RespCodeEnum.SUCCESS, "get1 success");
     }
 
+    @GetMapping("/get2")
+    public BaseResponse<String> get2(@RequestParam String name) {
+        return new BaseResponse<>(RespCodeEnum.SUCCESS, "get2" + name);
+    }
+
+    @GetMapping("/get3/{pathValue}")
+    public BaseResponse<String> get3(@PathVariable("pathValue") String value) {
+        return new BaseResponse<>(RespCodeEnum.SUCCESS, "get3" + value);
+    }
+
+    @PostMapping("/post1")
+    public BaseResponse<String> post1() {
+        return new BaseResponse<>(RespCodeEnum.SUCCESS, "post1");
+    }
+
+    @PostMapping("/post2")
+    public BaseResponse<DemoReq> post2(@RequestBody DemoReq demoReq) {
+        return new BaseResponse<>(RespCodeEnum.SUCCESS, demoReq);
+    }
 
 }
--- api-base/src/main/java/com/zhe/api/base/handler/ApiExceptionHandler.java
+++ api-base/src/main/java/com/zhe/api/base/handler/ApiExceptionHandler.java
@@ -3,12 +3,16 @@ package com.zhe.api.base.handler;
 import com.zhe.api.base.bean.BaseResponse;
 import com.zhe.api.base.constant.RespCodeEnum;
 import com.zhe.api.base.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler(BusinessException.class)
     @ResponseBody
@@ -19,6 +23,7 @@ public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public BaseResponse<?> processException(Exception e) {
+        LOGGER.error("system error, e:", e);
         return new BaseResponse<>(RespCodeEnum.SYSTEM_ERROR);
     }
 
